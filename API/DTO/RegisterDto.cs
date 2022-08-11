f6c431218fe3b45022371ba1b958385af99354b7
mfcgames <mfethicaliskan@gmail.com>
2022-08-11 15:31:16 +0300

    End of section 7

--- API/DTO/RegisterDto.cs
+++ API/DTO/RegisterDto.cs
@@ -13,6 +13,7 @@ namespace API.DTO
         [Required]
         public string UserName { get; set; }
         [Required]
+        [StringLength(8,MinimumLength =4)]
         public string Password  { get; set; }
     }
 }
